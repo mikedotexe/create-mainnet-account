b5b44338740727d53bf3ed63c52ebc2630426c63
Mike Purvis <mikedotexe@gmail.com>
2020-11-11 16:13:57 -0800

    initial commitial

---
+++ README.md
@@ -0,0 +1,31 @@
+A simple way to create NEAR mainnet accounts once you have an existing one.
+Exposes you to the merry ways of [near-api-js](https://www.npmjs.com/package/near-api-js) as well.
+Does not work with Ledger.
+
+Node version 12+, please.
+
+Before using, please install all npm packages with:
+
+    npm i
+
+### Usage
+
+Arguments:
+
+1. Existing mainnet account that will create the new account
+2. New mainnet account name that doesn't exist ([check if one exists](https://docs.near.org/docs/development/near-cli#near-state) with NEAR CLI)
+3. Public key (See [generate-key docs](https://docs.near.org/docs/development/near-cli#near-generate-key))
+
+```
+node create-mainnet-account.js creator.near newaccount.near P19ZkmB0H6hor3ZixxfCszgYn6beKqpkXa5553455g90
+```
+    
+### Troubleshooting
+
+If you're using NEAR CLI and thinks seem off, please make sure to prefix with the proper network. For example:
+
+    NEAR_ENV=mainnet near state mike.near
+    
+If the account is available, you'll receive a very fun error message that contains the words:
+
+>does not exist while viewing
---
+++ create-mainnet-account.js
@@ -0,0 +1,39 @@
+const { connect, keyStores, utils } = require('near-api-js');
+const path = require('path');
+const homedir = require('os').homedir();
+
+const CREDENTIALS_DIR = '.near-credentials';
+
+const nearConfig = {
+  networkId: 'mainnet',
+  nodeUrl: 'https://rpc.mainnet.near.org',
+  walletUrl: 'https://wallet.mainnet.near.org',
+  helperUrl: 'https://helper.mainnet.near.org'
+};
+
+if (process.argv.length === 2) {
+  console.info('Please enter in the form\nnode create-mainnet-account EXISTING_ACCOUNT.near NEW_ACCOUNT.near ED25519_PUBLIC_KEY\nIt will create an account with 3 Ⓝ if it\'s available');
+  return;
+}
+
+AccountGoBrrr = {
+  begin: async function() {
+    const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+    const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+    const near = await connect(Object.assign({ deps: { keyStore } }, nearConfig));
+    const creatorAccount = await near.account(process.argv[2]);
+
+    return await creatorAccount.functionCall(
+      'near',
+      'create_account',
+      {
+        new_account_id: process.argv[3],
+        new_public_key: process.argv[4]
+      },
+      '300000000000000',
+      utils.format.parseNearAmount('3')
+    )
+  }
+};
+
+AccountGoBrrr.begin();
